d1835f7dfa2924d91737f9c830d2ff1131c09cbe
Katelynn Thorn <thornka@oregonstate.edu>
2020-05-27 22:05:13 -0700

    Added ability to see reviews for a specific movie

--- userProfile.js
+++ userProfile.js
@@ -27,5 +27,34 @@ router.get('/', function(req, res){
         }
     });
 
+
+function getReviewFromTitle(req, res, mysql, context, complete) {
+var query = "SELECT review_movie_tv, review_title, review_body FROM review  WHERE review.review_movie_tv LIKE " + mysql.pool.escape(req.params.s);
+      console.log(query)
+
+      mysql.pool.query(query, function(error, results, fields){
+            if(error){
+                res.write(JSON.stringify(error));
+                res.end();
+            }
+            context.userProfile = results;
+            complete();
+     });
+ }
+
+router.get('/media_review/:s', function(req, res){
+        var callbackCount = 0;
+        var context = {};
+        var mysql = req.app.get('mysql');
+        getReviewFromTitle(req, res, mysql, context, complete);
+        function complete(){
+            callbackCount++;
+            if(callbackCount >= 1){
+                res.render('userProfile', context);
+            }
+        }
+    });
+
+
 return router;
 }();
