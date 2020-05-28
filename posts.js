388f43d02aa81b71a38a0ec4915180628c58f52e
Katelynn Thorn <thornka@oregonstate.edu>
2020-05-27 17:55:40 -0700

    Fixed posts functionality

--- posts.js
+++ posts.js
@@ -3,7 +3,7 @@ module.exports = function(){
     var router = express.Router();
 
     function getReviews(res, mysql, context, complete){
-        mysql.pool.query("SELECT review_id as id, review_movie_tv, review_title, review_body, review_author FROM review", function(error, results, fields){
+        mysql.pool.query("SELECT review_id as id, review_movie_tv, review_title, review_body FROM review", function(error, results, fields){
             if(error){
                 res.write(JSON.stringify(error));
                 res.end();
@@ -32,8 +32,8 @@ router.get('/', function(req, res){
 router.post('/', function(req, res){
         console.log(req.body)
         var mysql = req.app.get('mysql');
-        var sql = "INSERT INTO review (review_movie_tv, review_title, review_body, review_author) VALUES (?,?,?,?)";
-	var inserts = [req.body.review_movie_tv, req.body.review_title, req.body.review_body, req.body.review_author];
+        var sql = "INSERT INTO review (review_movie_tv, review_title, review_body) VALUES (?,?,?)";
+	var inserts = [req.body.review_movie_tv, req.body.review_title, req.body.review_body];
         sql = mysql.pool.query(sql,inserts,function(error, results, fields){
             if(error){
                 console.log(JSON.stringify(error))
